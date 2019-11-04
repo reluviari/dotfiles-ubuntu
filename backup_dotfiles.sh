148183d869f839b3039b63c19027ca0e5dd593f3
Danilo Dias <danilo@danilo.dias>
2019-11-04 13:13:59 -0300

    New backup 2019-11-04 13:13:59

--- backup_dotfiles.sh
+++ backup_dotfiles.sh
@@ -18,7 +18,7 @@ cp  $HOME/.config/Code/User/{settings.json} .
 cp -r $HOME/.config/Code/User/snippets .
 
 # copy list of extensions that currently installed
-code --list-extensions --show-versions >> ../../vscode/extensions.txt
+code --list-extensions --show-versions >> extensions.txt
 
 # copy other dot files 
 cp  $HOME/{.zshrc,.vimrc} .
